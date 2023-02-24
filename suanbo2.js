32b49f2773cfeee1f9f79cd67f4473509bcc7b03
박현지 <73213437+iamphj3@users.noreply.github.com>
2023-02-24 20:14:24 +0900

    Update suanbo2.js

--- suanbo2.js
+++ suanbo2.js
@@ -5,28 +5,40 @@ var array=[0,0,0,0,0,0,0,0,0];
  var count=0,flag=0;
  var i=0,ff=0;
 
-{ var question=["1. 충주 여행을 간다면 ",
+{ 
+ var question=
+ [
+ "1. 충주 여행을 간다면 ",
  "2. 숙소에서 뭐할래?",
  "3. 충주의 첫 번째 목적지는?",
  "4. 충주여행 기념품은?",
  "5. 충주 맛집에 간다면",
- "6. 잠시 카페에서 쉴 땐"];
+ "6. 잠시 카페에서 쉴 땐"
+ ];
 
- var pos=["계획 한 건 다 보고 갈래!",
+ var pos=
+[
+"계획 한 건 다 보고 갈래!",
 "숙소에서는 편하게 쉴래!",
 "핫플레이스에서 인생샷 건져야지!",
 "먹는 게 최고! 간식 잔뜩 사갈래",
 "인증샷은 필수! 남는 건 사진",
-"크고 깔끔한 프랜차이즈 카페"];
+"크고 깔끔한 프랜차이즈 카페"
+];
 
- var neg=["그냥 발길 닿는 대로 여행할래",
+ var neg=
+[
+"그냥 발길 닿는 대로 여행할래",
 "숙소에 그냥 있기 아까워! 야경 구경할래",
 "구경보단 맛집탐방 냠냠",
 "충주 감성 가득한 소품들",
 "사진은 뒷전! 먹는게 먼저",
-"충주 감성 가득한 개인 카페"];}
+"충주 감성 가득한 개인 카페"
+];
+}
 
-if(ff==0){
+if(ff==0)
+{
 $("#post").text(question[count]);
 $("#yes").text(pos[count]);
 $("#no").text(neg[count]);
@@ -67,7 +79,9 @@ $("#no").click(function () {
 	$("#yes").text(pos[count]);
 	$("#no").text(neg[count]);});}
 
-var resultFirst=[//result에 넣기
+var resultFirst=
+[
+ //result에 넣기
  "[물아일체형]",
  "[탕진형]",
  "[마이웨이형]",
@@ -76,7 +90,8 @@ var resultFirst=[//result에 넣기
  "[스펀지형]"
  ];
 
-var result=[
+var result=
+[
  "여행을 가서 가만히 앉아 경치 구경하는 걸 좋아하는 당신! 머릿속에 복잡한 생각들을 비워내는 중이군요. 조용한 시간이 필요한 만큼 여행을 가서도 당신만의 시간을 중요하게 생각하네요.",
  "비용이 들더라도 좋은 숙소를 잡고 좋은 음식을 먹고 싶어 하는 당신! 마음껏 먹고 마음껏 구매하는 재미로 스트레스를 해소하네요. 통장을 텅장으로 변신 시켜주는 당신은 진정한 탕진 마술사!",
  "여행의 로망은 즉흥적으로 즐기는 것이라고 생각하는 당신! 우연히 찾은 맛집에 뿌듯함을 느끼며 자유를 만끽합니다. 계획이 없기 때문에 언제든지 여행을 갈 준비가 되어 있네요. 자유로운 영혼에 박수를!",
@@ -87,6 +102,7 @@ var result=[
 
 var timer;
 var forFinal=0;
+	
 //yes,no에서 count가 6이면 호출할 함수
  function final() {
  	document.getElementById("img").src = "banner2.PNG";
@@ -114,12 +130,14 @@ var forFinal=0;
 //$("#text2").style.display='block';
 //	$("#text1").attr(display,"block");
 //	$("#text2").attr(display,"block");
+	 
  	var max=0,index=0;
  	for(var i=0;i<9;i++){
  		if(array[i]>max){
  			max=array[i];
  			index=i;}
  	}
+	 
  	$("#count").value=index;
 	//$("#form").submit();
 
@@ -136,6 +154,7 @@ var forFinal=0;
 	$("#btn1").width="40px";
 	$("#btn2").width="40px";
 	//$("").width="40px";
+	 
 	yes.fontSize="20px";
 	no.fontSize="20px";
 	yes.text("테스트 다시하기");
@@ -145,14 +164,18 @@ var forFinal=0;
 	$("#btn2").css("display","none");
 	$("#btn3").css("display","block");
 	$("#btn4").css("display","block");	
+	 
 	$("h1").css("display","block");
+	 
 	$("#answer").css("padding-bottom","0px");
+	 
 	$("#text1").text("여행 타입");
 	$("#text2").text("테스트 결과");
 	$("#text3").text("----------------------");
 	$("#text4").text("당신에게 맞는 여행 추천");
 	$("#post").text(resultFirst[index]+"\n\n\n");
 	$("#content").text(result[index]);
+	 
 	//	location.replace('eny	ouresult.html');
 }
 
